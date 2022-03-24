13dcb6a766cc9eed4e33d20c5edfe5b36e3ef401
misheck12 <misheck1720@gmail.com>
2022-03-24 14:38:31 +0200

    install webpack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import _ from 'lodash';
+import './style.css';
+import kebabMenu from './kebab-menu.svg';
+import enterIcon from './enter-icon.svg';
+import refreshIcon from './Refresh_icon.svg';
 
-function component() {
-    const element = document.createElement('div');
-  
-     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const toDoLists = [
+  {
+    description: 'Read my Bible',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Meet My coding partners',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Attend standup call',
+    completed: false,
+    index: 3,
+  },
+];
+
+const listItems = document.querySelector('.list-items');
+
+const refreshImg = document.querySelector('.refresh-icon');
+refreshImg.src = refreshIcon;
+
+const enterImg = document.querySelector('.enter-icon');
+enterImg.src = enterIcon;
+
+const populateLists = () => {
+  toDoLists.forEach((list) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'list-item';
+    listItem.innerHTML = ` <div>
+        <input type="checkbox"></input>
+        <p>${list.description}</p>
+      </div>
+      <img src="${kebabMenu}" alt="Kebab-menu" class="kebab-icon">
+    `;
+    
+    listItems.appendChild(listItem);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  populateLists();
+});
