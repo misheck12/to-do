6ae24b73e606ef0ff6455db35930294884302da2
misheck12 <misheck1720@gmail.com>
2022-03-25 22:04:38 +0200

    Fix linters

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import {
 import { loadStorage } from './modules/storage.js';
 import isComplete from './modules/state.js';
 
+
 import '@fortawesome/fontawesome-free/js/fontawesome.js';
-import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/solid.js'; 
 import '@fortawesome/fontawesome-free/js/regular.js';
 import '@fortawesome/fontawesome-free/js/brands.js';
 
