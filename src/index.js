20fbe997c97812a28fce01a97a26e64a64c5bbd2
misheck12 <misheck1720@gmail.com>
2022-03-25 12:39:37 +0200

    Fix the error

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import './style.css';
+import List from './list.js';
+
+const todoList = new List();
+todoList.display();
+// This line of code adds new Activity
+document.querySelector('#add-task').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const activity = e.target.elements.activity.value;
+  todoList.addActivity(activity);
+  e.target.reset();
+});
+
+// This line of clears completed Activities
+document.querySelector('.clear-completed').addEventListener('click', () => {
+  todoList.clearCompleted();
+});
+
+// This line of code clears all handler
+document.querySelector('#delete-all').addEventListener('click', () => {
+  todoList.clearAll();
+});
