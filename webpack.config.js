0c3d5690f34dfa4b6a7423421c5c0f5141392f67
misheck12 <misheck1720@gmail.com>
2022-03-25 20:44:55 +0200

    create add and remove function

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
+/* eslint-disable */
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
+module.exports = {
   mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
-   devServer: {
-    static: './dist',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-    plugins: [
-    new HtmlWebpackPlugin({
-    template: './src/index.html'
-    }),
-  ],
-   output: {
-     filename: 'main.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-
-   module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
+
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
         type: 'asset/resource',
       },
     ],
   },
- };
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
